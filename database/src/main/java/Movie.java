2512e70bea2b77e23e6ecb93e4d88feea423656c
Jonas Fredriksson <48363473+Miercolez@users.noreply.github.com>
2021-05-31 20:37:54 +0200

    Created Movie Entity with a MovieDao class. Also created methods to retrieve data from the database.

---
+++ database/src/main/java/Movie.java
@@ -0,0 +1,76 @@
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Movie {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String title;
+    private int length;
+    private String director;
+    private int releaseYear;
+
+    public Movie() {
+    }
+
+    public Movie(String title, int length, String producer, int releaseYear) {
+        this.title = title;
+        this.length = length;
+        director = producer;
+        this.releaseYear = releaseYear;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    public int getReleaseYear() {
+        return releaseYear;
+    }
+
+    public void setReleaseYear(int releaseYear) {
+        this.releaseYear = releaseYear;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    @Override
+    public String toString() {
+        return "Movie{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", length=" + length +
+                ", Producer='" + director + '\'' +
+                ", releaseYear=" + releaseYear +
+                '}';
+    }
+}
---
+++ database/src/main/java/MovieDao.java
@@ -0,0 +1,87 @@
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import java.util.List;
+
+public class MovieDao {
+
+    EntityManagerFactory emf = Persistence.createEntityManagerFactory("default");
+
+    public void addMoviePackToDatabase() {
+        List<Movie> movies = List.of(
+                new Movie("Sagan om ringen", 165, "Peter jackson", 2001),
+                new Movie("Pulp Fiction", 174, "Quentin Tarantino", 1994),
+                new Movie("Forest Gump", 165, "Robert Zemeckis", 1994),
+                new Movie("Matrix", 124, "Lana Wachowski", 1999),
+                new Movie("The Dark Knight", 152, "Christopher Nolan", 2008));
+
+        EntityManager em = emf.createEntityManager();
+
+        em.getTransaction().begin();
+        em.persist(movies);
+        em.getTransaction().commit();
+
+        em.close();
+    }
+
+    public List<Movie> getAllMovies() {
+        EntityManager em = emf.createEntityManager();
+
+        List<Movie> movies = em.createQuery("SELECT m FROM Movie m", Movie.class).getResultList();
+        em.close();
+
+        return movies;
+    }
+
+    public List<Movie> findMoviesByTitle(String title) {
+        EntityManager em = emf.createEntityManager();
+
+        List<Movie> movies = em.createQuery("SELECT m FROM Movie m WHERE m.title=:title", Movie.class).setParameter("title", title).getResultList();
+
+        em.close();
+
+        return movies;
+    }
+
+    public Movie findMovieById(Long id) {
+        EntityManager em = emf.createEntityManager();
+
+        Movie movies = em.createQuery("SELECT m FROM Movie m WHERE m.id=:id", Movie.class).setParameter("id", id).getSingleResult();
+
+        em.close();
+
+        return movies;
+    }
+
+    public List<Movie> findMoviesByYear(int releaseYear) {
+        EntityManager em = emf.createEntityManager();
+
+        List<Movie> movies = em.createQuery("SELECT m FROM Movie m WHERE m.releaseYear=:releaseYear", Movie.class).setParameter("releaseYear", releaseYear).getResultList();
+
+        em.close();
+
+        return movies;
+    }
+
+    public List<Movie> findMoviesByDirector(String director) {
+        EntityManager em = emf.createEntityManager();
+
+        List<Movie> movies = em.createQuery("SELECT m FROM Movie m WHERE m.director=:director", Movie.class).setParameter("director", director).getResultList();
+
+        em.close();
+
+        return movies;
+    }
+
+    public List<Movie> findMoviesByLength(int length) {
+        EntityManager em = emf.createEntityManager();
+
+        List<Movie> movies = em.createQuery("SELECT m FROM Movie m WHERE m.length=:length", Movie.class).setParameter("length", length).getResultList();
+
+        em.close();
+
+        return movies;
+    }
+
+
+}
