5f92a16c991bfcb5dbeb5d2b112db1059f6b22c0
Dennis Gustafsson <dennisgust@gmail.com>
2021-06-08 19:50:48 +0200

    LoadDB.java

--- handleRequest/src/main/java/module-info.java
+++ handleRequest/src/main/java/module-info.java
@@ -5,6 +5,6 @@ module handleRequest {
     requires utils;
     requires database;
     requires com.google.gson;
-    provides spi.Spi with GetAllMovies, AddMovie, FindMovieById, FindMovieByTitle, FindMovieByDirector, FindMovieByLength, FindMovieByReleaseYear, GetFile;
+    provides spi.Spi with GetAllMovies, AddMovie, FindMovieById, FindMovieByTitle, FindMovieByDirector, FindMovieByLength, FindMovieByReleaseYear, GetFile, LoadDB;
 
 }
